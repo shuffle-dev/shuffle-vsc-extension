d3be29ff34872928f2255f046a7791bfbb383c92
CoreAngel <bragon912@gmail.com>
2020-10-29 22:26:09 +0100

    Reformat tabs for spaces

--- src/extension.ts
+++ src/extension.ts
@@ -2,80 +2,80 @@ import * as vscode from 'vscode';
 
 
 export function activate(context: vscode.ExtensionContext) {
-	let disposable = vscode.commands.registerCommand('shuffle.openWindow', () => {
-		ShufflePanel.createOrShow(context.extensionUri);
-	});
+    let disposable = vscode.commands.registerCommand('shuffle.openWindow', () => {
+        ShufflePanel.createOrShow(context.extensionUri);
+    });
 
-	context.subscriptions.push(disposable);
+    context.subscriptions.push(disposable);
 
-	if (vscode.window.registerWebviewPanelSerializer) {
-		vscode.window.registerWebviewPanelSerializer(ShufflePanel.viewType, {
-			async deserializeWebviewPanel(webviewPanel: vscode.WebviewPanel, state: any) {
-				ShufflePanel.revive(webviewPanel, context.extensionUri);
-			}
-		});
-	}
+    if (vscode.window.registerWebviewPanelSerializer) {
+        vscode.window.registerWebviewPanelSerializer(ShufflePanel.viewType, {
+            async deserializeWebviewPanel(webviewPanel: vscode.WebviewPanel, state: any) {
+                ShufflePanel.revive(webviewPanel, context.extensionUri);
+            }
+        });
+    }
 }
 
 class ShufflePanel {
-	public static currentPanel: ShufflePanel | undefined;
+    public static currentPanel: ShufflePanel | undefined;
 
-	public static readonly viewType = 'shuffle';
+    public static readonly viewType = 'shuffle';
 
-	private readonly _panel: vscode.WebviewPanel;
-	private readonly _extensionUri: vscode.Uri;
-	private _disposables: vscode.Disposable[] = [];
+    private readonly _panel: vscode.WebviewPanel;
+    private readonly _extensionUri: vscode.Uri;
+    private _disposables: vscode.Disposable[] = [];
 
-	public static createOrShow(extensionUri: vscode.Uri) {
-		const column = vscode.ViewColumn.Beside;
+    public static createOrShow(extensionUri: vscode.Uri) {
+        const column = vscode.ViewColumn.Beside;
 
-		if (ShufflePanel.currentPanel) {
-			ShufflePanel.currentPanel._panel.reveal(column);
-			return;
-		}
+        if (ShufflePanel.currentPanel) {
+            ShufflePanel.currentPanel._panel.reveal(column);
+            return;
+        }
 
-		const panel = vscode.window.createWebviewPanel(
-			ShufflePanel.viewType, 'Shuffle', column, {
-				enableScripts: true,
+        const panel = vscode.window.createWebviewPanel(
+            ShufflePanel.viewType, 'Shuffle', column, {
+                enableScripts: true,
 
-				localResourceRoots: [vscode.Uri.joinPath(extensionUri, 'media')]
-			}
-		);
+                localResourceRoots: [vscode.Uri.joinPath(extensionUri, 'media')]
+            }
+        );
 
-		ShufflePanel.revive(panel, extensionUri);
-	}
+        ShufflePanel.revive(panel, extensionUri);
+    }
 
-	private constructor(panel: vscode.WebviewPanel, extensionUri: vscode.Uri) {
-		this._panel = panel;
-		this._panel.title = "Shuffle";
-		this._extensionUri = extensionUri;
+    private constructor(panel: vscode.WebviewPanel, extensionUri: vscode.Uri) {
+        this._panel = panel;
+        this._panel.title = "Shuffle";
+        this._extensionUri = extensionUri;
 
-		this._createPanel();
-		this._onChangeView();
-		this._onDispose();
-	}
+        this._createPanel();
+        this._onChangeView();
+        this._onDispose();
+    }
 
-	public static revive(panel: vscode.WebviewPanel, extensionUri: vscode.Uri) {
-		ShufflePanel.currentPanel = new ShufflePanel(panel, extensionUri);
-	}
+    public static revive(panel: vscode.WebviewPanel, extensionUri: vscode.Uri) {
+        ShufflePanel.currentPanel = new ShufflePanel(panel, extensionUri);
+    }
 
-	public dispose() {
-		ShufflePanel.currentPanel = undefined;
-		this._panel.dispose();
+    public dispose() {
+        ShufflePanel.currentPanel = undefined;
+        this._panel.dispose();
 
-		while (this._disposables.length) {
-			const elem = this._disposables.pop();
-			if (elem) {
-				elem.dispose();
-			}
-		}
-	}
+        while (this._disposables.length) {
+            const elem = this._disposables.pop();
+            if (elem) {
+                elem.dispose();
+            }
+        }
+    }
 
-	private _createPanel() {
-		const webview = this._panel.webview;
-		const nonce = getNonce();
+    private _createPanel() {
+        const webview = this._panel.webview;
+        const nonce = getNonce();
 
-		this._panel.webview.html = `<!DOCTYPE html>
+        this._panel.webview.html = `<!DOCTYPE html>
 			<html lang="en">
 			<head>
 				<meta charset="UTF-8">
@@ -92,32 +92,33 @@ class ShufflePanel {
 <!--				<script nonce="${nonce}" src=""></script>-->
 			</body>
 			</html>`;
-	}
+    }
 
-	private _onChangeView() {
-		this._panel.onDidChangeViewState(
-			e => {
-				if (this._panel.visible) {
-					this._createPanel();
-				}
-			},
-			null,
-			this._disposables
-		);
-	}
+    private _onChangeView() {
+        this._panel.onDidChangeViewState(
+            e => {
+                if (this._panel.visible) {
+                    this._createPanel();
+                }
+            },
+            null,
+            this._disposables
+        );
+    }
 
-	private _onDispose() {
-		this._panel.onDidDispose(() => this.dispose(), null, this._disposables);
-	}
+    private _onDispose() {
+        this._panel.onDidDispose(() => this.dispose(), null, this._disposables);
+    }
 }
 
 function getNonce() {
-	let text = '';
-	const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-	for (let i = 0; i < 32; i++) {
-		text += possible.charAt(Math.floor(Math.random() * possible.length));
-	}
-	return text;
+    let text = '';
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for (let i = 0; i < 32; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
 }
 
-export function deactivate() {}
+export function deactivate() {
+}
